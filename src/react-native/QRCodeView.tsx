1e410b1af0267a50dc2a7cdf76b35dafbe7e1f41
Jenea Vranceanu <jeneavranceanu@gmail.com>
2023-10-08 22:34:40 +0300

    feat(rn-component): adding a react native component

---
+++ src/react-native/QRCodeView.tsx
@@ -0,0 +1,37 @@
+import React, { useMemo } from "react";
+import { WebView } from "react-native-webview";
+import { Options } from "../types";
+import QRCodeStyling from "../core/QRCodeStyling";
+
+interface QRCodeViewProps {
+  options: Partial<Options>;
+}
+
+export const QRCodeView: React.FC<QRCodeViewProps> = ({ options }: QRCodeViewProps) => {
+  const html = useMemo(
+    () =>
+      `<html>
+      <head>
+        <script type="text/javascript" src="https://raw.githubusercontent.com/JeneaVranceanu/qr-code-styling/master/dist/qr-code-styling.js"></script>
+      </head>
+      <body>
+      <div id="canvas" style="background-color: #ab2323"></div>
+      <p> bull shit </p>
+      <script type="text/javascript">
+      ${new QRCodeStyling(options)}.append(document.getElementById("canvas"));
+      </script>
+      </body>
+      </html>`,
+    [options]
+  );
+  return (
+    <WebView
+      javaScriptEnabled={true}
+      originWhitelist={["*"]}
+      javaScriptEnabledAndroid={true}
+      source={{
+        html: html
+      }}
+    />
+  );
+};
